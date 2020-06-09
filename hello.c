cec4470b9fc55a089460ca120969f8f51e451446
mcase3 <54606922+mcase3@users.noreply.github.com>
2020-06-09 09:05:09 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -11,7 +11,9 @@
 
 int main(int argc, char **argv) {
 
-  printf("Markus Case, Andrew Aaldana\n");
+  printf("Markus Case, Andrew Aldana\n");
+  
+  printf("Mechanical Engineering\n");
 
   return 0;
 }
