5a89938a0db219e834954eaedcd2ff88c9b4bd4c
mcase3 <54606922+mcase3@users.noreply.github.com>
2020-06-09 09:02:14 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Markus Case, Andrew Aaldana
+ *
+ * Date: 2020/06/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Markus Case, Andrew Aaldana\n");
+
+  return 0;
+}
